ca43a80ed974b91c99c54ea5a903b38e9cce4f0a
Martin Emanuel <martinemanuel93@gmail.com>
2023-04-13 17:08:10 -0300

    fix featured

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -3,16 +3,16 @@ import { uuid } from '@utils';
 
 function Featured() {
   return (
-    <div className='m-4 px-4 overflow-x-scroll sm:overflow-x-auto'>
-      <div className='flex translate-x-[25%] sm:translate-x-0 sm:flex-wrap justify-around md:justify-between gap-2 my-4 pointer-events-none z-0'>
+    <div className='mt-12 px-4 overflow-x-scroll sm:overflow-x-auto'>
+      <div className='flex sm:flex-wrap justify-around md:justify-between gap-2 my-4 z-0'>
         {featuredData?.map((hotel, i) => (
-          <div key={uuid()} className='bg-base-100 shadow-lg'>
+          <div key={uuid()} className='translate-x-[55%] sm:translate-x-0 bg-base-100 shadow-lg box-border min-w-[10rem] max-w-[10rem] sm:max-w-[15rem]'>
             <img
               src={hotel.imgUrl}
               alt={hotel.city}
-              className='z-0 min-w-[10rem] max-w-[10rem] sm:max-w-[15rem] object-cover rounded-lg'
+              className='z-0  object-cover rounded-lg transition-all duration-500 hover:scale-105'
             />
-            <div className='pointer-events-none translate-x-4 -translate-y-[120%]  text-white text-xl sm:text-3xl font-bold '>
+            <div className='translate-x-4 h-0 relative -translate-y-20  text-white text-xl sm:text-3xl font-bold '>
               <h1>{hotel.city}</h1>
               <h2 className='dark:text-gray-200 z-0'>
                 {Math.floor(Math.random() * 9)} properties
